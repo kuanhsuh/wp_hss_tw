ece234cf60a9f8452ab5bbf98d080f84c52da2e9
dannyhuang <kuanhsuh@gmail.com>
2020-11-04 13:06:20 +0800

    Update Sidebar

--- template-parts/content-sidebar.php
+++ template-parts/content-sidebar.php
@@ -1,28 +1,21 @@
 <!--id 
-7/Wire Shelf
-14/SECURITY UNITS
-13/STORAGE SYSTEM
-11/Carts
-15/WORKBENCH
-16/PLATFORM TRUCK
-17/Dolly
-20/PIPE Fitting
-12/DRUM HEATER
-18/Hand Truck
-32/SPECIALITY HAND TRUCK
-33/HYDRAULIC SERIES
-19/Caster
+2/網片層架
+1/活動倉存
+3/工作桌類訂製品
+4/推車類訂製品
+5/存放櫃類訂製品
+6/特殊櫃體類訂製品
 -->
 
 <div class="col-md-3 sidebar">
-  <h3>Categories</h3>
+  <h3>分類</h3>
   <ul class="sidebar--list">
-    <li><a href="#">網片層架</a></li>
-    <li><a href="#">活動倉存</a></li>
-    <li><a href="#">工作桌類訂製品</a></li>
-    <li><a href="#">推車類訂製品</a></li>
-    <li><a href="#">存放櫃類訂製品</a></li>
-    <li><a href="#">特殊櫃體類訂製品</a></li>
+    <li><a href="<?php echo esc_url(get_category_link(2)); ?>">網片層架</a></li>
+    <li><a href="<?php echo esc_url(get_category_link(1)); ?>">活動倉存</a></li>
+    <li><a href="<?php echo esc_url(get_category_link(3)); ?>">工作桌類訂製品</a></li>
+    <li><a href="<?php echo esc_url(get_category_link(4)); ?>">推車類訂製品</a></li>
+    <li><a href="<?php echo esc_url(get_category_link(5)); ?>">存放櫃類訂製品</a></li>
+    <li><a href="<?php echo esc_url(get_category_link(6)); ?>">特殊櫃體類訂製品</a></li>
 
   </ul>
 </div>
